f655985400d962908bcf7b8d353b8b3cb2a60e45
Jay <kminet.jung@gmail.com>
2026-04-13 18:55:09 +0900

    v4.6: 결과 UI 통일 + 정제 청크 처리 + 빈 전사 수정

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v16';
+const CACHE = 'lispen-v17';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
