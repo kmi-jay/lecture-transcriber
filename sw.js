f152f1908d9eb826e7a5f82d2ad028bffdaae43d
Jay <kminet.jung@gmail.com>
2026-04-13 18:38:44 +0900

    v4.4: LLM 타임아웃/청크 안정성 + Q&A 버그 수정

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v14';
+const CACHE = 'lispen-v15';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
