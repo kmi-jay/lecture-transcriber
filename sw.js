83e744b17e23d499eacccc3efa5a1c820a07d0f8
Jay <kminet.jung@gmail.com>
2026-04-13 19:26:53 +0900

    v4.10: 실시간 경과 타이머 (전체 LLM 호출) + 타임아웃 3분 확대

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v20';
+const CACHE = 'lispen-v21';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
