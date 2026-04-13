ccf5c196489aa1d07f834a7947a8ffc3435e5778
Jay <kminet.jung@gmail.com>
2026-04-13 19:47:08 +0900

    v4.11: 노트/요약 품질 대폭 개선 (이중압축 제거, 프롬프트 강화, 토큰 스케일) + 정제본 저장 + ETA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v21';
+const CACHE = 'lispen-v22';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
