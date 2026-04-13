ad7e5bf9d9c0a1c3844034dd8eddb99cf2f20c2a
Jay <kminet.jung@gmail.com>
2026-04-13 16:37:44 +0900

    v4.1: fix all provider references, error modal, settings descriptions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v10';
+const CACHE = 'lispen-v11';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
