e77aa812c3f4182a6ec871b9a47f2811ddb3c8d0
Jay <kminet.jung@gmail.com>
2026-04-13 16:07:18 +0900

    v3.9: add output language selector for notes/summary/QA/refine

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v8';
+const CACHE = 'lispen-v9';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
