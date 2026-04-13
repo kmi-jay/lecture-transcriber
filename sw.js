64603f2d016a94c4f742e7f323a650243127bd68
Jay <kminet.jung@gmail.com>
2026-04-13 19:12:51 +0900

    v4.8: 전체 API 안정성 강화 (LLM_LIMITS 크래시, 전사 타임아웃, Claude 모델명, API 버전)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v18';
+const CACHE = 'lispen-v19';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
