e98fc663b7f76cf5542f6fcc7768413e9966aac0
Jay <kminet.jung@gmail.com>
2026-04-13 19:17:27 +0900

    v4.9: 크레딧 부족 오류 감지 및 안내 (billing 타입 분류, 즉시 중단, 충전 URL 안내)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v19';
+const CACHE = 'lispen-v20';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
