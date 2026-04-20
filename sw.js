f84853238176543fc62360121bcad80136e30c92
Jay <kminet.jung@gmail.com>
2026-04-20 23:22:13 +0900

    v4.17: Whisper 프롬프트 유출 근본 수정 + 환각 필터 v4 (7-pass)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v27';
+const CACHE = 'lispen-v28';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
