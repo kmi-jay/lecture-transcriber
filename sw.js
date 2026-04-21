2ae9960ce8c5be863ee806b2fcf0f01c296b8b68
Jay <kminet.jung@gmail.com>
2026-04-21 13:25:55 +0900

    v4.18: 정제 프롬프트 대폭 강화 + 복합 환각 필터 + 선행 노이즈 정리

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v28';
+const CACHE = 'lispen-v29';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
