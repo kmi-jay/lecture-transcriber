aee4bf1daea2c5c9e13624506bc073f547934307
Jay <kminet.jung@gmail.com>
2026-04-21 15:55:30 +0900

    v4.19: Google Cloud STT (Chirp 2) 프로바이더 추가 — Plaud 동일 엔진 + 정제 프롬프트 v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v29';
+const CACHE = 'lispen-v30';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
