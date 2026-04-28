d40a90d5bfb826f392cb86e3cef87b4bd6b24b0b
Jay <kminet.jung@gmail.com>
2026-04-29 00:41:03 +0900

    v4.23: 청크 0 버그 수정 + 비용 함수 안전성 + 폴백 UI 갱신 + 청크 precheck 이동

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v33';
+const CACHE = 'lispen-v34';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
