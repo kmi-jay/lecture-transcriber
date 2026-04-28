384058039503d29b9e26b89d88062f0fe72f86cf
Jay <kminet.jung@gmail.com>
2026-04-28 23:33:24 +0900

    v4.21: 오토 모드 확장 + diarization 강제전환 버그 수정 + 비용 표시 단가·총액 형식

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v31';
+const CACHE = 'lispen-v32';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
