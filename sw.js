7a9346eb7a0a2e62048a7c2d1a3c848fe961f1e1
Jay <kminet.jung@gmail.com>
2026-04-29 13:11:57 +0900

    v4.28: 품질우선 폴백 오류 + Google STT 청크 크기 + 에러 메시지 수정

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v38';
+const CACHE = 'lispen-v39';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
