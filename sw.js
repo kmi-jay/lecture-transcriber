4e82ce6b9ad7807f203e6f3687e6017cea8e9091
Jay <kminet.jung@gmail.com>
2026-05-04 18:00:52 +0900

    v4.31: Google STT 청크 5분→3분 (base64 크기 계산 수정)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v41';
+const CACHE = 'lispen-v42';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
