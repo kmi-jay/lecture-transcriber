f4b1c435db278e4fd262d8aa264bf2233287c496
Jay <kminet.jung@gmail.com>
2026-05-04 17:41:22 +0900

    v4.30: Groq LLM 입력 한도 축소 (50K→30K, 무료 티어 413 방지)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v40';
+const CACHE = 'lispen-v41';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
