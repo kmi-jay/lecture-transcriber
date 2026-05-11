3fce593a776db801e9724e5e7137327ed205b796
Jay <kminet.jung@gmail.com>
2026-05-11 23:06:07 +0900

    v4.39: 외부 전사본 불러오기 (텍스트 임포트 → LLM 후처리)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v48';
+const CACHE = 'lispen-v50';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
