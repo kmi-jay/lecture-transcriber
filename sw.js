9fb6eb34031c4fa2f3a46ac8aa6f8f655f63e28d
Jay <kminet.jung@gmail.com>
2026-05-18 14:03:06 +0900

    v4.43: fix post-processing buttons unresponsive after text import

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v53';
+const CACHE = 'lispen-v54';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
