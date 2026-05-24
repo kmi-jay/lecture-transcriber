e7dc305d38c51eba2dc1cc5fdaebf34c208d0984
Jay <kminet.jung@gmail.com>
2026-05-24 16:33:01 +0900

    v4.47: 다국어 정제 재발방지 — 전체 한국어 번역 강제 + 타임스탬프 임의 삽입 금지

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Lispen Service Worker v5
-const CACHE = 'lispen-v57';
+const CACHE = 'lispen-v58';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
